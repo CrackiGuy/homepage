7974a03178357a0af16ae1d12a0a1324fabffac8
Cracki <naylinaung.nla.xo@gmail.com>
2019-09-30 14:48:36 +0630

    css

--- sonicstar/js/custom.js
+++ sonicstar/js/custom.js
@@ -1,27 +1,27 @@
 var fromCity = $("#onedept");
 var toCity = $("#onearr");
-var submit=$("#onewaysubmit");
+var submit = $("#onewaysubmit");
 
 
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-    toCity.attr("disabled", true);
-    // submit.attr("disabled",true);
-  
-    firstcondition();
+//     toCity.attr("disabled", true);
+//     // submit.attr("disabled",true);
 
-});
+//     firstcondition();
 
-fromCity.on("change",function(){
+// });
+
+fromCity.on("change", function() {
 
     toCity.attr("disabled", false);
     firstcondition();
 
     let $boxval = $("#onedept").val();
 
-    if ($boxval==="origin") {
+    if ($boxval === "origin") {
 
-      
+
 
     } else {
 
@@ -29,193 +29,192 @@ fromCity.on("change",function(){
             let ele = $("#onearr > option").eq(ind);
             if (ele.val() === $boxval) ele.attr("disabled", "disabled");
             else ele.removeAttr("disabled");
-          });
+        });
     }
 
- 
 
-  
+
+
 
 });
 
-    toCity.on("change",function(){
-        let $boxval = $("#onearr").val();
-        firstcondition();
+toCity.on("change", function() {
+    let $boxval = $("#onearr").val();
+    firstcondition();
 
-        if ($boxval === "origin") {
-            
-        
-            $('#onedept option:eq(0)').prop('selected', true).trigger('change');
-
-           
-        } else {
-            $("#onedept > option").each(function(ind) {
-                let ele = $("#onedept > option").eq(ind);
-                if (ele.val() === $boxval) ele.attr("disabled", "disabled");
-                else ele.removeAttr("disabled");
-             });
-        }
-
-   
-    });
+    if ($boxval === "origin") {
 
 
-$( document ).ready(function() {
+        $('#onedept option:eq(0)').prop('selected', true).trigger('change');
 
-    var total=$("#total_quantity").val();
-    var currentVal=1;
-    
-    $(".plus").click( function(e) {
+
+    } else {
+        $("#onedept > option").each(function(ind) {
+            let ele = $("#onedept > option").eq(ind);
+            if (ele.val() === $boxval) ele.attr("disabled", "disabled");
+            else ele.removeAttr("disabled");
+        });
+    }
+
+
+});
+
+
+$(document).ready(function() {
+
+    var total = $("#total_quantity").val();
+    var currentVal = 1;
+
+    $(".plus").click(function(e) {
 
         e.preventDefault();
-        
+
         // Define field variable
         field = "input[name=" + $(this).attr("field") + "]";
-        
+
         // Get its current value
         var currentVal = parseInt($(field).val());
-        
-        
+
+
         // If is not undefined
-        if ( !isNaN(currentVal) && currentVal < 10 && total < 9) {
-    
+        if (!isNaN(currentVal) && currentVal < 10 && total < 9) {
+
             // Increment
             $(field).val(currentVal + 1);
             $("#adult_quantity").val(currentVal + 1);
 
             total++;
-         
-         
-          
+
+
+
         }
-        
+
     });
-    
+
     // This button will decrement the value till 0
-    $(".minus").click( function(e) {
-    
+    $(".minus").click(function(e) {
+
         e.preventDefault();
-        
+
         // Define field variable
         field = "input[name=" + $(this).attr("field") + "]";
-        
+
         // Get its current value
         var currentVal = parseInt($(field).val());
-        
+
         // If it isn't undefined or its greater than 0
-        if ( !isNaN(currentVal) && currentVal > 1 ) {
+        if (!isNaN(currentVal) && currentVal > 1) {
             // Decrement one
             $(field).val(currentVal - 1);
             $("#adult_quantity").val(currentVal - 1);
             total--;
         }
-        
+
     });
 
 
-    $(".child_plus").click( function(e) {
+    $(".child_plus").click(function(e) {
 
         e.preventDefault();
-        
+
         // Define field variable
         field = "input[name=" + $(this).attr("field") + "]";
-        
+
         // Get its current value
         var currentVal = parseInt($(field).val());
-        
+
         // If is not undefined
-        if ( !isNaN(currentVal) && currentVal < 10 && total < 9) {
-    
+        if (!isNaN(currentVal) && currentVal < 10 && total < 9) {
+
             // Increment
             $(field).val(currentVal + 1);
             $("#child_quantity").val(currentVal + 1)
             total++;
         }
-        
+
     });
-    
+
     // This button will decrement the value till 0
-    $(".child_minus").click( function(e) {
-    
+    $(".child_minus").click(function(e) {
+
         e.preventDefault();
-        
+
         // Define field variable
         field = "input[name=" + $(this).attr("field") + "]";
-        
+
         // Get its current value
         var currentVal = parseInt($(field).val());
-        
+
         // If it isn't undefined or its greater than 0
-        if ( !isNaN(currentVal) && currentVal > 0 ) {
+        if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
             $(field).val(currentVal - 1);
             $("#child_quantity").val(currentVal - 1)
             total--;
         }
-        
+
     });
 
 
 
-    $(".infant_plus").click( function(e) {
+    $(".infant_plus").click(function(e) {
 
         e.preventDefault();
-        
+
         // Define field variable
         field = "input[name=" + $(this).attr("field") + "]";
-        
+
         // Get its current value
         var currentVal = parseInt($(field).val());
-        
+
         // If is not undefined
-        if ( !isNaN(currentVal) && currentVal < 10 && total < 9) {
-    
+        if (!isNaN(currentVal) && currentVal < 10 && total < 9) {
+
             // Increment
             $(field).val(currentVal + 1);
             $("#infant_quantity").val(currentVal + 1);
 
             total++;
-    
+
         }
-        
+
     });
-    
+
     // This button will decrement the value till 0
-    $(".infant_minus").click( function(e) {
-    
+    $(".infant_minus").click(function(e) {
+
         e.preventDefault();
-        
+
         // Define field variable
         field = "input[name=" + $(this).attr("field") + "]";
-        
+
         // Get its current value
         var currentVal = parseInt($(field).val());
-        
+
         // If it isn't undefined or its greater than 0
-        if ( !isNaN(currentVal) && currentVal > 0 ) {
+        if (!isNaN(currentVal) && currentVal > 0) {
             // Decrement one
             $(field).val(currentVal - 1);
             $("#infant_quantity").val(currentVal - 1);
             total--;
         }
-        
+
     });
 
 });
 
-function firstcondition(){
+function firstcondition() {
 
 
-    var from_original=$("#onedept").val();
-    var to_original=$("#onearr").val();
+    var from_original = $("#onedept").val();
+    var to_original = $("#onearr").val();
 
-    if (from_original=="origin" || to_original=="origin") {
+    if (from_original == "origin" || to_original == "origin") {
 
         submit.attr("disabled", true);
-      
-    }else{
+
+    } else {
         submit.attr("disabled", false);
     }
 
 }
-
