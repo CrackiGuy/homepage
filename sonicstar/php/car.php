b43cfffd9079fc3785849bc48af5c5f873441005
mohag <mohmohaung1010@gmail.com>
2019-10-17 13:39:34 +0630

    Fixing

--- sonicstar/php/car.php
+++ sonicstar/php/car.php
@@ -41,13 +41,14 @@
     <div class="form-group col-md-12 col-lg-3">
 
       <select name="car_city" id="car_city_daily1" class="select2class">
-
-        <?php foreach ($result1 as $city) { ?>
-              <option value="<?php echo $city['id']; ?>"><?php echo $city['title']; ?></option>
+        <?php foreach ($result1 as $city) {
+                    if($city['id']==89){ ?>
+                        <option value="<?php echo $city['id']; ?>" selected="selected"><?php echo $city['title']; ?></option>
+                    <?php }else{ ?>
+                        <option value="<?php echo $city['id']; ?>"><?php echo $city['title']; ?></option>
+                    <?php } ?>
         <?php } ?>
-             </select>
-
-
+        </select>
     </div>
     <div class="form-group col-md-12 col-lg-3">
             <input type="text" class="car-date" name="daily[pickup_date]" id="daily_pickup_date" placeholder="Pick Up Date">
@@ -79,15 +80,17 @@
 			</div>
 			<div class="form-group col-md-12 col-lg-3">
         <select name="car_city" id="car_city_daily" class="select2class">
-          <option value="default" selected="selected">Pickup City</option>
-          <?php foreach ($result1 as $city) { ?>
-            <option value="<?php echo $city['id']; ?>"><?php echo $city['title']; ?></option>
+          <?php foreach ($result1 as $city) {
+            if($city['id']==89){ ?>
+                <option value="<?php echo $city['id']; ?>" selected="selected"><?php echo $city['title']; ?></option>
+            <?php }else{ ?>
+                <option value="<?php echo $city['id']; ?>"><?php echo $city['title']; ?></option>
+            <?php } ?>
           <?php } ?>
         </select>
       </div>
 			<div id="dest" class="form-group col-md-12 col-lg-3">
 					<select name="daily[destination]" id="destination" class="select2class" >
-            <option value="default" selected="selected">Pickup City</option>
             <?php foreach ($result2 as $city) { ?>
                 <option value="<?php echo $city['id']; ?>"><?php echo $city['title']; ?></option>
               <?php } ?>
@@ -118,17 +121,17 @@
       <div class="modal-body">
         <div class="container">
         <div id="timechoose">
-         
-        
-   
+
+
+
               <!-- <div class="col-6 card border-success">
-                  From Time 
+                  From Time
               </div>
               <div class="col-6">
-                  To Time  
+                  To Time
               </div> -->
         </div>
-        </div>  
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger mx-auto col-4" id="removebtntime" data-dismiss="modal">Applying </button>
@@ -175,12 +178,12 @@
               </div>
             </div>
         </div>
-      
+
 
       </div>
 
       <div class="modal-footer">
-        <button type="button" id="remove" class="btn btn-secondary mx-auto col-4" data-dismiss="modal">Close</button>      
+        <button type="button" id="remove" class="btn btn-secondary mx-auto col-4" data-dismiss="modal">Close</button>
       </div>
     </button>
   </div>
